b5802d8773615a8872680fdcea72303d142cc509
Sn00kY89 <56257657+Sn00kY89@users.noreply.github.com>
2026-02-05 23:21:57 +0100

    Fix dark mode tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // MODIFICA: Questa riga abilita il dark mode manuale tramite classe
+  darkMode: 'class', 
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
